8073134d9aea5f52e79579902e86106e4d3fb7ac
JONATHAN EASTIN <j.d.east@icloud.com>
2017-12-19 01:13:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-let a = 5;
-let b = 5;
-let n = '5';
-
 function add(a, b) {
   return a + b;
 }
 
-function multiply(a, b) {
-  return a * b;
+function subtract() {
+  return a - b;
 }
 
-function subtract(a, b) {
-  return a - b;
+function multiply(a, b) {
+  return a * b;
 }
 
 function divide(a, b) {
@@ -25,15 +21,3 @@ function inc(n) {
 function dec(n) {
   return --n;
 }
-
-function makeInt(n) {
-  return parseInt(n);
-}
-
-console.log(add(a, b));
-console.log(multiply(a, b));
-console.log(subtract(a, b));
-console.log(divide(a, b));
-console.log(inc(n));
-console.log(dec(n));
-console.log(makeInt(n));
